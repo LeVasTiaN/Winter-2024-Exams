4c97bf17659c1ae5efded68f788064702f6d8642
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 00:11:35 +0100

    Finished refactoring count.js
    Lessened the variable usage by integrating the variable values directly into the loop.

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,11 +2,9 @@
 
 const count = (obj) => {
   let sum = 0;
-  const keys = Object.keys(obj);
-  for (const key of keys) {
-    const value = obj[key];
-    if (typeof value === 'number')
-      sum += value;
+  for (const key of Object.keys(obj)) {
+    if (typeof obj[key] === 'number')
+      sum += obj[key];
   };
   return sum;
 };
