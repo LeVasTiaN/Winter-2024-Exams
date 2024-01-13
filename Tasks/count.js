5623dcb403ebf6a5b6d5379199a85e1427f3b717
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-13 22:22:15 +0100

    Removed unnecessary lines, added const and let, respected code style conventions.

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,13 @@
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
+const count = (obj) => {
+  let sum = 0;
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
+    if (typeof value === 'number')
+      sum += value;
   });
-  ({});
   return sum;
 };
 
