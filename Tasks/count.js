93840010fd8e505936bebcdc76659fed0f51695f
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-13 23:43:49 +0100

    Added 'use strict' and changed the .forEach loop into for .. of loop.

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,13 +1,13 @@
-// Sum all number values in dict
+'use strict';
 
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number')
       sum += value;
-  });
+  };
   return sum;
 };
 
