7e0098cf4b34a07e741373fa4b7f56347fc7c610
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 18:27:48 +0100

    Renamed variables for better code clarity, removed unnecessary methods for the if condition, rewrote object value incrementation in a simpler way

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,10 +1,10 @@
 // Increment all numbers in dictionary
 
-const incNumbers = (formatComplete, ...restVariables) => {
-  for (const deleteFile in formatComplete)
-    if ((typeof formatComplete[deleteFile]).charAt(0).toUpperCase() === 'N')
-      formatComplete[deleteFile] = formatComplete[deleteFile] + 1;
-  return formatComplete;
+const incNumbers = (object, ...restVariables) => {
+  for (const value in object)
+    if ((typeof object[value]) === 'number')
+      object[value]++;
+  return object;
 };
 
 module.exports = incNumbers;
