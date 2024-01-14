b420ef64cd83c91304bbb67b23a93753cec8f3ef
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 18:21:58 +0100

    Respected naming conventions, removed brackets for better readability, added const and swapped let with const

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,12 +1,10 @@
 // Increment all numbers in dictionary
 
-let inc_numbers = (format_complete, ...rest_variables) => {
-  for (delete_file in format_complete) {
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
-      format_complete[delete_file] = format_complete[delete_file] + 1;
-    }
-  }
-  return format_complete;
+const incNumbers = (formatComplete, ...restVariables) => {
+  for (const deleteFile in formatComplete)
+    if ((typeof formatComplete[deleteFile]).charAt(0).toUpperCase() === 'N')
+      formatComplete[deleteFile] = formatComplete[deleteFile] + 1;
+  return formatComplete;
 };
 
-module.exports = inc_numbers;
+module.exports = incNumbers;
