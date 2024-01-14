c1ad4b72586634778f200c6ae1c2e601d057aeb0
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 19:30:18 +0100

    Followed naming conventions, added const, renamed the function for better clarity

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,17 +1,17 @@
 // Validate person name
 
-const isValidate = (T) => {
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
+const isValid = (t) => {
+  if (!t) return false;
+  if (t === '') return false;
+  if (typeof t !== 'string') return false;
+  if (t.length === 0) return false;
+  if (!t.includes(' ')) return false;
   {
-    for (C of T) {
-      if (C === ' ') continue;
+    for (const c of t) {
+      if (c === ' ') continue;
       if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
+        c.toLowerCase().charCodeAt(0) >= 97 &&
+        c.toLowerCase().charCodeAt(0) <= 122
       ) {
       } else {
         return false;
@@ -21,4 +21,4 @@ const isValidate = (T) => {
   }
 };
 
-module.exports = isValidate;
+module.exports = isValid;
