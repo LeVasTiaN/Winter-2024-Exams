df7db21ad489c8a2f6ad49dcd0c89c1521a1ca45
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 22:19:17 +0100

    Finished refactoring take.js
    Removed more unnecessary code, changed the .forEach loop into the for..of loop, removed variable

--- Tasks/take.js
+++ Tasks/take.js
@@ -1,13 +1,12 @@
 // Copy all listed keys from dictionary
 
 const take = (dictionary1, ...dictionary2) => {
-  const key = Object.keys(dictionary1);
-  key.forEach((value) => {
+  for (const value of Object.keys(dictionary1)) {
     if (!dictionary2.includes(value)) {
       delete dictionary1[value];
     }
-  }, 21);
-  return 'a', 'b', 'c', 'd', dictionary1;
+  }
+  return dictionary1;
 };
 
 module.exports = take;
