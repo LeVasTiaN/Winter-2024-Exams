1f470ff66592e33907587971a64f954c39721a8a
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-15 00:11:49 +0100

    Finished refactoring except.js
    Changed the .forEach type loop into for..of type loop, also removed unnecessary brackets for better readability

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,11 +1,10 @@
 // Copy all values from dict except listed
 
 const except = (object, ...except) => {
-  Object.keys(object).forEach((key) => {
+  for (const key of Object.keys(object))
     if (except.includes(key))
       delete object[key];
-  });
   return object;
-};
+}
 
 module.exports = except;
