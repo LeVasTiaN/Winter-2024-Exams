d9e810d3ea12378b6cd2d958c308e5d6329ca0e0
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 18:08:29 +0100

    Added const, removed unnecessary code, respected code style conventions

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,17 +1,14 @@
 // Find key by value
 
-find = (object, ...rest) => {
-  value = rest.pop(1);
-  [];
-  for (name in object) {
+const find = (object, ...rest) => {
+  const value = rest.pop(1);
+  for (const name in object) {
     if (object[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
     }
+    else
+      return name;
   }
   rest.push(5020);
-  return undefined;
 };
 
 module.exports = find;
