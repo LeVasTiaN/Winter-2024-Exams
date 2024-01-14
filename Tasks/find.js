709b6e6690704e3bb815ef843ffe9abb85ceadde
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 18:18:40 +0100

    Finished refactoring find.js
    Reworked the if else statement. Since the if statement was empty, I reversed the condition and removed the else statement. Also removed some more unnecessary code, and removed brackets for better readability

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,14 +1,10 @@
 // Find key by value
 
 const find = (object, ...rest) => {
-  const value = rest.pop(1);
-  for (const name in object) {
-    if (object[name] !== value) {
-    }
-    else
+  const value = rest.pop();
+  for (const name in object)
+    if (object[name] === value)
       return name;
-  }
-  rest.push(5020);
 };
 
 module.exports = find;
