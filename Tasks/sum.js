7644439791b0951029a4e67988d04f813b2bf1c7
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 16:12:14 +0100

    Removed unnecessary code, added const and let

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,9 +1,8 @@
 // Sum all numbers from an array
 
 const k = (sum = function (s = () => {}) {
-  sum = [0];
-  let k = 5;
-  for (i of s) {
+  let sum = [0];
+  for (const i of s) {
     let t = typeof i;
     if (t === 'number') {
       if (sum.length > 0) {
@@ -12,9 +11,7 @@ const k = (sum = function (s = () => {}) {
       }
     }
   }
-  sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
 });
 
 module.exports = sum;
