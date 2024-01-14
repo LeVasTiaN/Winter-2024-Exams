a8f02bf7d65e63588285d019fc3c492981250e2a
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 03:34:52 +0100

    Finished refactoring split.js
    rewrote the function a little, for better readability and consistency

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,8 +1,9 @@
 // Split an array into two parts
 'use strict';
 
-const split_Array = (index, array) => {
-  return [array.slice(0, index), array.slice(index)];
-};
+const split_Array = (index, array) => [
+  array.slice(0, index),
+  array.slice(index)
+];
 
 module.exports = split_Array;
