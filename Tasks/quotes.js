dce1144b908be0923c3004ad1daa27da0d583d47
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-15 00:34:03 +0100

    Finished refactoring quotes.js
    Renamed variables for better clarity and readability, added 'use strict'

--- Tasks/quotes.js
+++ Tasks/quotes.js
@@ -1,18 +1,19 @@
 // Change double quotation to open or close quotation
+'use strict';
 
 
-const quotes = (s) => {
-  let res = [];
-  let open = false;
-  for (const c of s) {
-    if (c === '"') {
-      res.push(open ? '»' : '«');
-      open = !open;
+const quotes = (string) => {
+  let result = [];
+  let flag = false;
+  for (const character of string) {
+    if (character === '"') {
+      result.push(flag ? '»' : '«');
+      flag = !flag;
     }
     else
-      res.push(c);
+      result.push(character);
   }
-  return res.join('');
-};
+  return result.join('');
+}
 
 module.exports = quotes;
