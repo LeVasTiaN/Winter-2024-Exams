dc51729cf88a1c47364e63096ff3586935e3654d
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 17:52:12 +0100

    Finished refactoring words.js
    Used a different approach and rewrote the redundant "if" statements into just three methods

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,28 +1,8 @@
 // Count words in a string
+'use strict';
 
 const words = (string) => {
-  let number_of_words_in_s = 0;
-  let flag = false;
-  for (const character of string) {
-    if (!flag) {
-      if (character === ' ') {
-        if (flag === true)
-          flag = false;
-      }
-      else {
-        if (flag === false)
-          flag = true;
-        number_of_words_in_s++;
-      }
-    }
-    else {
-      if (character === ' ') {
-        if (flag === true)
-          flag = false;
-      }
-    }
-  }
-  return number_of_words_in_s;
+  return string.split(' ').filter(Boolean).length;
 };
 
 module.exports = words;
