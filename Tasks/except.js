7203042b97bf6b8ad418e608fecf080cd580664a
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-15 00:09:21 +0100

    Removed variable

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,8 +1,7 @@
 // Copy all values from dict except listed
 
 const except = (object, ...except) => {
-  const keys = Object.keys(object);
-  keys.forEach((key) => {
+  Object.keys(object).forEach((key) => {
     if (except.includes(key))
       delete object[key];
   });
