2de7d08957d9fe753e1e6811cb1498262d120a08
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-14 19:36:51 +0100

    simplified the if statements, removed unnecessary brackets

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,24 +1,24 @@
 // Validate person name
 
 const isValid = (t) => {
-  if (!t) return false;
-  if (t === '') return false;
-  if (typeof t !== 'string') return false;
-  if (t.length === 0) return false;
-  if (!t.includes(' ')) return false;
-  {
+  if (
+    typeof t !== 'string' ||
+    t.length === 0 ||
+    !t.includes(' ')
+  )
+    return false;
     for (const c of t) {
       if (c === ' ') continue;
       if (
-        c.toLowerCase().charCodeAt(0) >= 97 &&
-        c.toLowerCase().charCodeAt(0) <= 122
+        c.toLowerCase() >= 'a' &&
+        c.toLowerCase() <= 'z'
       ) {
-      } else {
+      }
+      else {
         return false;
       }
     }
     return true;
-  }
 };
 
 module.exports = isValid;
