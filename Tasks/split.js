b42afd1ade6e0aedcd8523bb25ed9da2739a4a8e
LeVasTiaN <lavrik.2005inbox@gmail.com>
2024-01-15 02:09:35 +0100

    Fixed an error in split.js

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,9 +1,9 @@
 // Split an array into two parts
 'use strict';
 
-const splitArray = (index, array) => {
+const splitArray = (index, array) => [
   array.slice(0, index),
   array.slice(index)
-}
+]
 
 module.exports = splitArray;
